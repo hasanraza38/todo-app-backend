aed7aa231cdc7244a23d34227d362de02999c26d
hasanraza38 <92hasanraza689@gmail.com>
2024-11-19 02:06:45 +0500

    updates

--- index.js
+++ index.js
@@ -1,10 +1,63 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 const app = express()
-const port = 3000
+const port = process.env.PORT;
+
+app.use(express.json());
+
+
+const users = [
+  {
+
+    title:"hsan",
+    id : Date.now(),
+  }
+];
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+// add new user 
+
+
+app.post("/user" , (req , res) => {
+  const {title } = req.body;
+  if (!title) {
+    res.status(400).json({
+      message : "title required"
+    })
+    return;
+  }
+  
+  users.push({
+    title,
+    id : Date.now(),
+  })
+
+  res.status(201).json({
+    message :"user is created",
+    data : users,
+  });
+  
+})
+
+
+//get all users
+
+app.get("/users" ,(req, res) =>{
+  res.status(200).json({
+    data : users
+  })
+})
+
+
+// app.get('/about', (req, res) => {
+//   res.send('Hello about!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
