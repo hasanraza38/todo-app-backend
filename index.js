d1648608218fea374f1123da1c0c9a42b47f513e
hasanraza38 <92hasanraza689@gmail.com>
2025-01-05 02:40:34 +0500

    updates

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import connectDB from "./src/db/index.js";
 import todosRoutes from "./src/routes/todos.routes.js";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
@@ -22,7 +22,7 @@ app.use("/api/v1", todosRoutes);
 
 connectDB()
   .then(() => {
-    app.listen(procces.env.PORT, () => {
+    app.listen(port, () => {
       console.log(`⚙️  Server is running at port : ${port}`);
     });
   })
