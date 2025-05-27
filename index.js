4170e7a02be04d0b797486582c3034381f744bd3
HASAN RAZA <92hasanraza689@gmail.com>
2025-05-27 14:52:00 +0500

    updates

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import express from "express";
 import connectDB from "./src/db/index.js";
 import todosRoutes from "./src/routes/todos.routes.js";
+import serverlessExpress from "@vendia/serverless-express";
 
 const app = express();
 const port = process.env.PORT;
@@ -20,12 +21,32 @@ app.get("/", (req, res) => {
 app.use("/api/v1", todosRoutes);
 // routes
 
-connectDB()
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`⚙️  Server is running at port : ${port}`);
-    });
-  })
-  .catch((err) => {
-    console.log("MONGO DB connection failed !!! ", err);
-  });
+
+
+
+// Do NOT call app.listen — instead, export a handler
+let server;
+
+const connectAndExport = async () => {
+  await connectDB();
+  server = serverlessExpress({ app });
+};
+
+connectAndExport();
+
+export const handler = async (event, context) => {
+  if (!server) {
+    await connectAndExport();
+  }
+  return server(event, context);
+};
+
+// connectDB()
+//   .then(() => {
+//     app.listen(port, () => {
+//       console.log(`⚙️  Server is running at port : ${port}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("MONGO DB connection failed !!! ", err);
+//   });
